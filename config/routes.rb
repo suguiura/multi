43f1e33dd75694d1b16a333ebf50e6600f001c94
Rafael Suguiura <rafael.suguiura@gmail.com>
2010-04-19 10:34:55 -0300

    Session scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :sessions
+
   map.resources :machines
 
   # The priority is based upon order of creation: first created -> highest priority.
