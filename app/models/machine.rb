84642b43c49de7ff089663a03576a94cc46e2081
Rafael Suguiura <rafael.suguiura@gmail.com>
2010-05-10 22:30:51 +0000

    Average work time per day for each machine and refactoring

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -6,11 +6,23 @@ class Machine < ActiveRecord::Base
   validates_numericality_of :availableTime, :only_integer => true
   
   def total_session_time
-    self.sessions.sum('strftime("%s",end) - strftime("%s",start)')
+    total = 0
+    self.sessions.each { |session| total += session.duration.to_i }
+    total
   end
   
   def average_session_time
-    self.sessions.average('strftime("%s",end) - strftime("%s",start)')
+    total_session_time / self.sessions.size
+  end
+  
+  def average_work_time_per_day
+    set = Set.new
+    self.sessions.each { |session| set << session.start.to_date }
+    total_session_time / set.size
+  end
+  
+  def utilization_percent_per_day
+    average_work_time_per_day.to_f / availableTime
   end
   
   def xls_column_names
