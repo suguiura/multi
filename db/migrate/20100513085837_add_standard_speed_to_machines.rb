740136f3d350605161020558bb84f731602db348
Rafael Suguiura <rafael.suguiura@gmail.com>
2010-05-14 15:58:24 +0000

    Default value for Standard Speed column in Machines

--- db/migrate/20100513085837_add_standard_speed_to_machines.rb
+++ db/migrate/20100513085837_add_standard_speed_to_machines.rb
@@ -1,6 +1,6 @@
 class AddStandardSpeedToMachines < ActiveRecord::Migration
   def self.up
-    add_column :machines, :standard_speed, :float
+    add_column :machines, :standard_speed, :float, :default => 0
   end
 
   def self.down
